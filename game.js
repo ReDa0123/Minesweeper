e55781d135075e52cae19b654d211f3ca7d2932d
ReDa0123 <89853677+ReDa0123@users.noreply.github.com>
2021-10-04 15:05:11 +0200

    Update game.js

--- game.js
+++ game.js
@@ -78,6 +78,7 @@ document.addEventListener("DOMContentLoaded", () => {
         const j = Math.floor(Math.random() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]];
     }
+    return array
 }
 
   function placeNumbers(){
